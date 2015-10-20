540b150b841c8989b8386bce8245396393b2c206
Adrian Rogers <adritek@gmail.com>
2015-10-21 00:23:23 +1000

    cut out verbose functions

--- scriptz.js
+++ scriptz.js
@@ -1,40 +1,33 @@
 var bullshit = "Applications,incentivize,e-tailers,cutting-edge,e-services,functionalities,e-markets,action-items,social,innovative,wikis,integrateAJAX-enabled,Revolutionary,A-list,initiatives,post,dynamic,metrics,revolutionary,value-added,optimize,value-added,reinvent,integrate,unleash,grow,Long-tail,communities,functionalities,mission-critical,holistic,target,beta-test,turnkey,embedded,end-to-end,wireless,aggregate,viral,content,scale,enterprise,streamline,addelivery,streamline,podcasting,Real-time,vortals,e-enable,webservices,architectures,orchestrate,streamline,models,monetize,embedded,virtual,users,sticky,networking,incubate";
-var comma = ",";
 var $boxes = $(".box");
 var grid = 9;
-/* functions */
-
-// split the string into an array of words
-function splitString() {
-  function arrayOfStrings(stringToSplit, separator){
-    return stringToSplit.split(separator);
-  };
-  return arrayOfStrings(bullshit, comma);
-};
-
-var splitwords = splitString();
-splitwords.forEach(function(element, index, array){
-  // console.log(index + ": " + element);
-});
-// pick 9 random numbers
 var randNum;
 var gridNums = [];
+
+/* functions */
+var splitwords = bullshit.split(","); // array
+
+// pick 9 random numbers
 for(var i = 0; i < grid; i++){
   randNum = Math.floor(Math.random()* splitwords.length);
   gridNums.push(randNum);
 }
-console.log(gridNums);
-function pickRand(wordsArr){
 
-  // var rand = wordsArr[Math.floor(Math.random() * wordsArr.length)];
-  // console.log(rand);
+// match the numbers from splitwords
+for (var i = 0; i < splitwords.length; i++){
+  for (var j = 0; j < gridNums.length; j++){
+    if (splitwords[i] == gridNums[j]){
+      console.log("match");
+    }
+  }
 }
 
+/*TODO*/
 // place words into the grid
 // select 9 words
 
 /* logging */
-// console.log(splitString(bullshit, comma));
-// console.log(pickWords(splitString());
-// console.log($boxes.length);
+console.log("The string is now in an array of: " + splitwords.length + " elements");
+console.log("We now have " + gridNums.length + " random numbers from the array length " + gridNums.sort());
+console.log("There are: " +$boxes.length + " boxes on the screen");
 // console.log(splitString());
