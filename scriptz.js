9b144a9cdb24acb3c1e8db317298251b59ec5b10
Adrian Rogers <adritek@gmail.com>
2015-11-26 23:25:01 +1000

    first refactor - it works

--- scriptz.js
+++ scriptz.js
@@ -1,4 +1,4 @@
-var bullshit = "Applications,incentivize,e-tailers,cutting-edge,e-services,functionalities,e-markets,action-items,social,innovative,wikis,integrateAJAX-enabled,Revolutionary,A-list,initiatives,post,dynamic,metrics,revolutionary,value-added,optimize,value-added,reinvent,integrate,unleash,grow,Long-tail,communities,functionalities,mission-critical,holistic,target,beta-test,turnkey,end-to-end,wireless,aggregate,viral,content,scale,enterprise,streamline,addelivery,streamline,podcasting,Real-time,vortals,e-enable,webservices,architectures,orchestrate,streamline,models,monetize,embedded,virtual,users,sticky,networking,incubate";
+var bullshit = "cloud,moving forward,ROI,blue sky,applications,aw sheeeeit,incentivize,e-tailers,cutting-edge,e-services,functionalities,e-markets,action-items,social,innovative,wikis,integrateAJAX-enabled,Revolutionary,A-list,initiatives,post,dynamic,metrics,revolutionary,value-added,optimize,value-added,reinvent,integrate,unleash,grow,Long-tail,communities,functionalities,mission-critical,holistic,target,beta-test,turnkey,end-to-end,wireless,aggregate,viral,content,scale,enterprise,streamline,addelivery,streamline,podcasting,Real-time,vortals,e-enable,webservices,architectures,orchestrate,streamline,models,monetize,embedded,virtual,users,sticky,networking,incubate";
 var $boxes = $(".box");
 var grid = 9;
 var randNum;
@@ -22,41 +22,78 @@ $boxes.each(function(index){
 // Set a listener to check off a word when it is clicked
 function notify(){
   $(this).toggleClass("checked");
-  // console.log($(this).text());
-  checkLine($(this));
+  checkRow($(this));
+  checkColumn($(this));
+  checkDiagonal($(this));
+  fullCard($(this));
 }
 $(".box").on("click", notify);
 
-// Check if a line is checked
-function checkLine($element){
-  // check for row bingo TODO: use modulus for different grids - saw it on codepen somewhere
-  var winners = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8]
-  ]
+// Check if a diagonal is selected
+function checkDiagonal(el){
+  var count = 0;
+  var box = $(".box");
+  if (box.eq(0).hasClass("checked")&&box.eq(4).hasClass("checked")&&box.eq(8).hasClass("checked")){
+    console.log("WIN on diagonal - top left to bottom right");
+    endGame();
+  } else if (box.eq(2).hasClass("checked")&&box.eq(4).hasClass("checked")&&box.eq(6).hasClass("checked")){
+    console.log("WIN on diagonal - top right to bottom left");
+    endGame();
+  }
+}
 
-  var possibleWinners = winners.length;
-
-  console.log("Selected boxes in array: " + selected.length);
-
-  selected.push($element);
-
-  // Compare winners array to selected array for matches
-  for (var i = 0; i < possibleWinners; i++) {
-    var cellExists = 0;
-
-    for (var j = 0; j < 3; j++) {
-      if ($.inArray(winners[i][j], selected) > -1) {
-          cellExists++;
+// Check if a column is selected
+function checkColumn(el){
+  var count1 = 0, count2 = 0, count3 = 0;
+  var c1 = $(".c1"), c2 = $(".c2"), c3 = $(".c3");
+  for (var i = 0; i < c1.length; i++) {
+    if(c1.eq(i).hasClass('checked')){
+      count1 += 1;
+      if(count1 == 3){
+        console.log("COLUMN WIN");
+        endGame();
+      }
+    } else if (c2.eq(i).hasClass('checked')) {
+      count2 += 1;
+      if(count2 == 3){
+        console.log("COLUMN WIN");
+        endGame();
+      }
+    } else if (c3.eq(i).hasClass('checked')) {
+      count3 += 1;
+      if(count3 == 3){
+        console.log("COLUMN WIN");
+        endGame();
       }
     }
+  }
+}
 
-    // If all 5 winner cells exist in selected array alert success message
-    if (cellExists == 3) {
-      alert('Winner!');
+// Check if a line is selected
+function checkRow(el){
+  // check for row bingo TODO: http://codepen.io/anon/pen/vBJhH - modulus!
+  var row = el.parent().children();
+  var count = 0;
+
+  for (var i = 0; i < row.length; i++) {
+    if(row.eq(i).hasClass('checked')){
+      count += 1;
+      if(count == 3){
+        console.log("ROW WIN");
+        endGame();
+      }
     }
   }
-  var index = $( ".box" ).index($element); //for logging
-  console.log( "That was div index #" + index );
+}
+
+// Check if a full card is selected
+function fullCard(el){
+  if ($(".checked").length == 9){
+    console.log("FULL CARD");
+  }
+}
+
+function endGame(){
+  // $("div").removeClass("checked");
+  console.log("calls endGame");
 }
